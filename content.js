f85bb4968d4356798af1edbf548e13f4cfb2b86a
Slothy <105455001+TheRealSlothy@users.noreply.github.com>
2026-01-28 14:25:52 +0000

    Add files via upload

---
+++ content.js
@@ -0,0 +1,28 @@
+﻿console.log("NMS Has Loaded");
+
+// Debug logging
+function debugLog(message) {
+    chrome.storage.local.get(['debugMode'], (data) => {
+        if (data.debugMode) {
+            console.log(`[NMS DEBUG] ${message}`);
+        }
+    });
+}
+
+//Shorts
+function isYouTubeShorts() {
+    const isShorts = window.location.pathname.includes('/shorts/');
+    debugLog(`Checked if on Shorts page: ${isShorts}`);
+    return isShorts;
+}
+
+function checkAntiBrainRot() {
+    chrome.storage.local.get('antiBrainRot', (data) => {
+        if (data.antiBrainRot && isYouTubeShorts()) {
+            debugLog("AntiBrain Rot active: redirecting to YouTube home");
+            window.location.href = 'https://www.youtube.com/';
+        }
+    });
+}
+
+setInterval(checkAntiBrainRot, 250);
---
+++ popup.js
@@ -0,0 +1,26 @@
+﻿const toggle = document.getElementById('toggle1');
+const toggle2 = document.getElementById('toggle2');
+
+
+// Load setting on popup open
+chrome.storage.local.get('debugMode', (data) => {
+    toggle.checked = !!data.debugMode;
+});
+
+chrome.storage.local.get('antiBrainRot', (data) => {
+    toggle2.checked = !!data.antiBrainRot;
+});
+
+// Listen for toggle changes
+toggle.addEventListener('change', function () {
+    const isDebug = this.checked;
+    chrome.storage.local.set({ debugMode: isDebug });
+    console.log('Debug Mode:', isDebug);
+});
+
+toggle2.addEventListener('change', function () {
+    const isAntiBrainRot = this.checked;
+    chrome.storage.local.set({ antiBrainRot: isAntiBrainRot });
+    console.log('AntiBrain Rot:', isAntiBrainRot);
+});
+
